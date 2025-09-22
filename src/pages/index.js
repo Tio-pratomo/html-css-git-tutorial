20748a2d86086f4c42e59eb402b7f09285b53dfa
sulis <sulistiopratomo@gmail.com>
2025-09-22 12:33:50 +0700

    edit: metadata dan title

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,8 +33,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`${siteConfig.title}`}
+      description="Tutorial web development lengkap yang mencakup HTML, CSS, dan Git, cocok untuk pemula."
     >
       <HomepageHeader />
       <main>
